9f14133d94a8fd79a58a7e3b61f49f7bf7391b02
tleveque <thierry.leveque@askida.com>
2018-11-22 10:52:20 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Car {
+  vin: string;
+  year: number;
+  brand: string;
+  color: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public queuedFromFilter: Date;
+  public filterMaxDateValue: Date;
+  public defaultDate: Date;
+  public queuedToFilter: Date;
+  public startedFromFilter: Date;
+  public startedToFilter: Date;
+  public completedToFilter: Date;
+  public completedFromFilter: Date;
+
+  public createdByFilter: string;
+
+  cars: Car[];
+
+  public ngOnInit(): void {
+
+    this.filterMaxDateValue = new Date();
+    this.defaultDate = new Date();
+
+    this.cars = [];
+
+    this.cars.push(
+      {
+        vin: 'fsdgf',
+        year: 2018,
+        brand: 'Nissan',
+        color: 'red'
+      }
+    );
+  }
+
+  public filterDateColumn(queuedFrom: string, contains: string) {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule, OverlayPanelModule } from 'primeng/primeng';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TableModule,
+    ButtonModule,
+    OverlayPanelModule,
+    CalendarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
